a07b35aec6cf55215327c5653cd5fc8c71173d80
digital-programmer <mohandibya123@gmail.com>
2022-01-08 22:59:23 +0530

    Design admin Page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,37 @@
-module.exports.home = function (req, res) {
+const City = require('../models/city');
+
+module.exports.home = async function (req, res) {
     if (!req.isAuthenticated()) {
         return res.redirect("/users/sign-in");
     }
-    return res.render("home", {
-        title: "Home",
-    });
+
+    if (req.user.role === "admin") {
+        try {
+            const city = await City.findOne({ _id: req.user.id });
+            let placeName = "";
+            if (city) {
+                placeName = city["city"];
+                return res.render("admin_page", {
+                    title: "Admin Page",
+                    place: placeName
+                })
+            }
+            return res.render("admin_page", {
+                title: "Admin Page",
+                place: "",
+            })
+        } catch (err) {
+            req.flash('error', "Error in finding city name");
+            return res.render("admin_page", {
+                title: "Admin Page",
+                place: "",
+            })
+        }
+
+    } else {
+        return res.render("user_page", {
+            title: "User Page",
+        });
+    }
+
 };
